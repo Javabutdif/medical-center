a83b710a4e77d4768d403a9444573e290bb3938b
Anton James Genabio <jamesgenabio31@gmail.com>
2025-01-15 16:41:04 +0800

    feat []: env established

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,7 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { test } from "./api/admin";
 
 function App() {
-  const [data, setData] = useState("");
-  const getData = async () => {
-    const response = await test();
-    setData(response);
-  };
-  useEffect(() => {
-    getData();
-  });
-  return <>{data}</>;
+  return <></>;
 }
 
 export default App;
