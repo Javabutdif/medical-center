bb17a71add500026e7687860bc2609ea39be7355
Anton James Genabio <jamesgenabio31@gmail.com>
2025-01-15 16:24:53 +0800

    feat []: env established

--- client/src/connections/server_connection.js
+++ client/src/connections/server_connection.js
@@ -1,3 +1,3 @@
 export const server_connection = () => {
-  return "http://localhost:5000";
+  return import.meta.env.VITE_API_URL;
 };
--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.use(express.json());
+
+app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.CORS,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
